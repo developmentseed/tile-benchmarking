992c45b120fe6e8629969d2c5fc2d6bca7648bab
Aimee Barciauskas <aimee@developmentseed.org>
2023-08-24 17:35:52 -0700

    Refactoring fake data tests

--- zarr_helpers.py
+++ zarr_helpers.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import numpy as np
+import s3fs
 import traceback
 
 def open_dataset(dataset_url, reference: bool = False, anon: bool = True, multiscale: bool = False, z: int = 0):
@@ -24,66 +25,6 @@ def open_dataset(dataset_url, reference: bool = False, anon: bool = True, multis
         print(f"Failed to open {dataset_url} with error {e}")
         traceback.print_exc()
     return ds
-
-# From https://github.com/carbonplan/cmip6-downscaling/blob/main/cmip6_downscaling/methods/common/utils.py#L189
-def calc_auspicious_chunks_dict(
-    da: xr.DataArray,
-    target_size_bytes: int,
-    chunk_dims: tuple = ("lat", "lon")
-) -> dict:
-    """Figure out a chunk size that, given the size of the dataset, the dimension(s) you want to chunk on
-    and the data type, will fit under the target_size. Currently only works for 100mb which
-    is the recommended chunk size for dask processing.
-
-    Parameters
-    ----------
-    da : xr.DataArray
-        Dataset or data array you're wanting to chunk
-    chunk_dims : tuple, optional
-        Dimension(s) you want to chunk along, by default ('lat', 'lon')
-
-    Returns
-    -------
-    chunks_dict : dict
-        Dictionary of chunk sizes with the dimensions not listed in `chunk_dims` being the
-        length of that dimension (avoiding the shorthand -1 in order to play nice
-        with rechunker)
-    """
-    if not isinstance(chunk_dims, tuple):
-        raise TypeError(
-            "Your chunk_dims likely includes one string but needs a comma after it! to be a tuple!"
-        )
-    dim_sizes = dict(zip(da.dims, da.shape))
-
-    # initialize chunks_dict
-    chunks_dict = {}
-
-    # dims not in chunk_dims should be one chunk (length -1), since these ones are going to
-    # be contiguous while the dims in chunk_dims will be chunked
-    for dim in dim_sizes.keys():
-        if dim not in chunk_dims:
-            # we'll only add the unchunked dimensions to chunks_dict right now
-            # rechunker doesn't like the the shorthand of -1 meaning the full length
-            # so we'll always just give it the full length of the dimension
-            chunks_dict[dim] = dim_sizes[dim]
-    data_bytesize = da.dtype.itemsize
-    # calculate the size of the smallest minimum chunk based upon dtype and the
-    # length of the unchunked dim(s). chunks_dict currently only has unchunked dims right now
-    smallest_size_one_chunk = data_bytesize * np.prod([dim_sizes[dim] for dim in chunks_dict])
-
-    # the dims in chunk_dims should be of an array size (as in number of elements in the array)
-    # that creates ~100 mb. `perfect_chunk` is the how many of the smallest_size_chunks you can
-    # handle at once while still staying below the `target_size_bytes`
-    perfect_chunk = target_size_bytes / smallest_size_one_chunk
-    # then make reasonable chunk size by rounding up (avoids corner case of it rounding down to 0...)
-    # but if the array is oblong it might get big (? is logic right there- might it get small??)
-    perfect_chunk_length = int(np.ceil(perfect_chunk ** (1 / len(chunk_dims))))
-    for dim in chunk_dims:
-        # check that the rounding up as part of the `perfect_chunk_length` calculation
-        # didn't make the chunk sizes bigger than the array itself, and if so
-        # clip it to that size
-        chunks_dict[dim] = min(perfect_chunk_length, dim_sizes[dim])
-    return chunks_dict
 
 def get_chunk_size(ds: xr.DataArray): 
     chunks = ds.encoding.get("chunks", "N/A")
@@ -121,3 +62,117 @@ def get_dataset_specs(collection_name: str, source: str, variable: str, ds: xr.D
         'compression': compression
     }
 
+def generate_data_store(
+    xdim: int,
+    ydim: int,
+    multiple: int = 0,    
+    time_steps: int = 1,
+) -> xr.Dataset:
+    if multiple:
+        # expand grid by multiple
+        data_values_per_chunk = ydim * xdim * multiple
+        # to maintain the aspect ratio, where we know size == y * x and x = 2y
+        ydim = round(np.sqrt(data_values_per_chunk/2))
+        xdim = 2*ydim
+    data = np.random.random(size=(time_steps, ydim, xdim))
+
+    # Create Xarray datasets with dimensions and coordinates
+    ds = xr.Dataset({
+        'data': (['time', 'lat', 'lon'], data),
+    }, coords={
+        'time': np.arange(time_steps),
+        'lat': np.linspace(-90, 90, ydim),
+        'lon': np.linspace(-180, 180, xdim)
+    })
+    return ds, xdim, ydim
+
+def generate_multiple_datastores(
+    n_multiples: int,
+    xdim: int, # starting x dimension
+    ydim: int, # starting y dimension
+    store_directory: str,
+    s3_filesystem: s3fs.S3FileSystem,
+    target_chunks: dict = None,
+    multiple: int = 2,
+) -> None:  
+    for n_multiple in range(n_multiples):
+        multiple_arg = 0 if n_multiple == 0 else multiple
+        ds, xdim, ydim = generate_data_store(xdim, ydim, multiple_arg)
+
+        try:
+            if target_chunks:
+                ds = ds.chunk(target_chunks)
+            else:
+                ds = ds.chunk({
+                    'time': 1, 
+                    'lat': ydim, 
+                    'lon': xdim
+                })
+            path = f'{store_directory}/store_lat{ydim}_lon{xdim}.zarr'
+            store = s3fs.S3Map(root=path, s3=s3_filesystem, check=False)
+            print(f"Writing to {path}")
+            ds.to_zarr(store, mode='w')
+        except Exception as e:
+            print(e)
+
+def get_dataset_specs_from_directory(zarr_directories: list, variable: str = 'data'):
+    datastore_specs = {}
+    # Loop through each item and open it with xarray if it's a Zarr store
+    for path in zarr_directories:
+        try:
+            # get the dataset specs
+            ds = open_dataset(f"s3://{path}")
+            dataset_name = path.split('/')[-1]
+            ds_specs = get_dataset_specs(dataset_name, f"s3://{path}", variable, ds)            
+            datastore_specs[dataset_name] = ds_specs
+            # product of all dimensions / 
+            number_of_chunks = round(np.prod(list(ds_specs['shape'].values())) / np.prod(list(ds_specs['chunks'].values())))
+            ds_specs['number_of_chunks'] = number_of_chunks
+        except Exception as e:
+            # Print an error message if unable to open the Zarr store
+            print(f"Could not open {item} as a Zarr store. Error: {e}")
+    return datastore_specs
+
+### Tile test helpers
+import morecantile
+import random
+from profiler.main import Timer
+from titiler_xarray.titiler.xarray.reader import ZarrReader
+tms = morecantile.tms.get("WebMercatorQuad")
+
+def generate_random_tile(z):
+    random_lat = random.randint(-85, 85)
+    random_lon = random.randint(-175, 175)
+    tile = tms.tile(random_lon, random_lat, z)
+    return (tile.x, tile.y, tile.z)
+
+def time_tile_generation(zoom: tuple, source: str, variable: str):
+    x, y, z = generate_random_tile(zoom)
+    with Timer() as t:
+        with ZarrReader(
+            source,
+            variable=variable,
+            reference=False
+        ) as src_dst:
+            image = src_dst.tile(
+                x,
+                y,
+                z,
+                tilesize=256,
+            )
+    return round(t.elapsed * 1000, 2) 
+
+def run_tests(datastore_specs: dict, zooms: list, niters: int = 2, variable: str = 'data'):
+    for item in datastore_specs.keys():
+        datastore_specs[item]['all tile times'] = {}
+        datastore_specs[item]['mean tile time'] = {}    
+        for zoom in zooms:
+            tests_key = f'{zoom} tests'
+            datastore_specs[item]['all tile times'][tests_key] = []
+            test_results = datastore_specs[item]['all tile times'][tests_key]
+            source = datastore_specs[item]['source']
+            for i in range(niters):
+                tile_time = time_tile_generation(zoom, source, variable)
+                test_results.append(tile_time)
+            datastore_specs[item]['mean tile time'][zoom] = np.mean(test_results)
+    return datastore_specs
