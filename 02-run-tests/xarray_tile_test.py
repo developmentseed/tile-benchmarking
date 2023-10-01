c6dc96d7e7af569494f506ffdc7604a052904ee5
Aimee Barciauskas <aimee@developmentseed.org>
2023-10-01 22:04:32 +0000

    CMIP6 kerchunk and modeling

--- 02-run-tests/xarray_tile_test.py
+++ 02-run-tests/xarray_tile_test.py
@@ -1,6 +1,7 @@
 from test import Test, timer_decorator
 import math
 import numpy as np
+from rioxarray.exceptions import RioXarrayError
 import sys; sys.path.append('..')
 from titiler_xarray.titiler.xarray.reader import xarray_open_dataset, ZarrReader
 import helpers.zarr_helpers as zarr_helpers
@@ -41,7 +42,12 @@ class XarrayTileTest(Test):
             reference=self.reference,
             group=group
         ) as src_dst:
-            image = src_dst.tile(
-                *xyz_tile,
-                tilesize=256,
-            ) 
+            try:
+                image = src_dst.tile(
+                    *xyz_tile,
+                    tilesize=256,
+                )
+            # TODO: Resolve why we are getting "Transformed bounds crossed the antimeridian. Please transform your bounds manually using rasterio.warp.transform_bounds and clip using the bounding box(es) desired."
+            except RioXarrayError as e:
+                print(f"skipping this tile {xyz_tile} for {self.dataset_url} due to error: {e}")
+                pass
