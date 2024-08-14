8117a860e1335c506eec6877e53f29f2cc2a8351
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-08-14 01:18:59 +0000

    Time tile generation with lambda using locust

--- 02-run-tests/cmr_tile_test.py
+++ 02-run-tests/cmr_tile_test.py
@@ -3,9 +3,16 @@ from titiler.cmr.backend import CMRBackend
 from titiler.cmr.reader import ZarrReader
 from titiler.cmr import __version__ as titiler_cmr_version
 from rio_tiler.models import ImageData
+import psutil
 
 
-"""Create map tile."""
+def get_size(bytes, suffix="B"):
+    """Format bytes."""
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
 
 
 def tile(
@@ -18,6 +25,7 @@ def tile(
     reader_options: dict,
     **kwargs,
 ) -> tuple[ImageData, list[str]]:
+    """Create map tile."""
     backend = CMRBackend(reader=ZarrReader, reader_options=reader_options)
     cmr_query["concept_id"] = concept_id
 
@@ -30,6 +38,9 @@ class CMRTileTest(Test):
         self.cmr_query = cmr_query
         self.reader_options = {"variable": self.variable}
         self.titiler_cmr_version = titiler_cmr_version
+        self.physical_cores = psutil.cpu_count(logical=False)
+        self.total_cores = psutil.cpu_count(logical=True)
+        self.memory = get_size(psutil.virtual_memory().total)
 
     def generate_arguments(self, batch_size: int = 1, zoom: int = 0):
         return [self.generate_random_tile(zoom) for i in range(batch_size)]
